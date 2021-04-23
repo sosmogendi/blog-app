72a3ad91ea1607020497abf7ecc69bf6097c7021
Sos Mogendi <sosmogendi@gmail.com>
2021-04-23 22:18:42 +0300

    Heroku config files and updates

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!$3-$!&3!#u*+%wnm4a+0j1w_s*(+qfqaug4m88@nz7u3jzfrr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
